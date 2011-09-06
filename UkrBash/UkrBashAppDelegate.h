436d08d9dc2c2f83a1480976fe8eaf759785f77b
Maks Markovets <maks.markovets@gmail.com>
2011-09-07 00:31:59 +0300

    Initial Commit

---
+++ UkrBash/UkrBashAppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  UkrBashAppDelegate.h
+//  UkrBash
+//
+//  Created by Maks Markovets on 9/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UkrBashAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
