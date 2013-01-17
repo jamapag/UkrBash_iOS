3eda870394e32959bfb6cc9767dab609300b379e
Michail Grebionkin <m.grebionkin@smile2mobile.net>
2013-01-17 20:16:56 +0200

    disable portrait upside down orientation

--- UkrBash/UBViewController.m
+++ UkrBash/UBViewController.m
@@ -62,7 +62,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;// (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 @end
